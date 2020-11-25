167e1f4df48c7d768a5f0ddc1d81b6760ee70a02
sdetAutomation <sdet.testautomation@gmail.com>
2020-11-25 17:38:37 -0600

    feat: add app properties, add user model, add data.sql, and update readme

---
+++ src/main/java/com/sdet/auto/users/model/User.java
@@ -0,0 +1,104 @@
+package com.sdet.auto.users.model;
+
+import javax.persistence.*;
+
+// Entity refers to the name of the class.  Represents a table that is stored in a database.
+// Defaults to name of class, however, can also declare a different name @Entity(name = "YourName")
+@Entity
+@Table(name = "users") // this will be the name of the table.  Defaults to the entity name if name field not defined
+public class User {
+
+    @Id // this annotation will make variable as a primary key
+    @GeneratedValue
+    private Long id;
+
+    // defaults to field name if you do not define name)
+    @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    private String firstname;
+
+    @Column(name = "LAST_NAME", length = 50, nullable = false)
+    private String lastname;
+
+    @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "ROLE", length = 50, nullable = false)
+    private String role;
+
+    // No Argument Constructor
+    public User() {
+
+    }
+
+    public User(Long id, String username, String firstname, String lastname, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
