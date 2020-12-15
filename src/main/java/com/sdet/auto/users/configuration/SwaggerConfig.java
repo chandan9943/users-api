4e814bcea311daa25e31b64a1c6e90b18698e2f8
sdetAutomation <sdet.testautomation@gmail.com>
2020-12-14 19:44:27 -0600

    feat: add AppInfo class and method to SwaggerConfig

--- src/main/java/com/sdet/auto/users/configuration/SwaggerConfig.java
+++ src/main/java/com/sdet/auto/users/configuration/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.sdet.auto.users.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,11 +17,24 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
+
     // swagger metadata: http://localhost:8080/v2/api-docs
     // swagger ui url: http://localhost:8080/swagger-ui.html
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("sdetAutomation's User Api")
+                .description("This page lists all User Api's")
+                .version("2.0")
+                .contact(new Contact("sdetAutomation", "https://github.com/sdetAutomation/", "sdet.testautomation@gmail.com"))
+                .license("MIT License")
+                .licenseUrl("https://github.com/sdetAutomation/users-api/blob/main/LICENSE")
+                .build();
+    }
 }
