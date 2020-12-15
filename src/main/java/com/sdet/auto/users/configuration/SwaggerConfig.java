e0b365d31fbdcba6d743fa55243931bc697493de
sdetAutomation <sdet.testautomation@gmail.com>
2020-12-14 21:33:28 -0600

    feat: add bean validation package, add max constaint to dto, import bean validator to SwaggerConfig class

--- src/main/java/com/sdet/auto/users/configuration/SwaggerConfig.java
+++ src/main/java/com/sdet/auto/users/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.sdet.auto.users.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +13,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Import(BeanValidatorPluginsConfiguration.class)
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
