dedf339971da371d3f3a77cae6fd9401056f6781
idexcelsiordev <id.excelsior.dev@gmail.com>
2021-07-21 21:05:19 -0500

    dfasd

---
+++ bin/main/data.sql
@@ -0,0 +1,3 @@
+insert into users values(101, 'darth.vader@gmail.com', 'darth', 'vader', 'villain', 'darth.vader');
+insert into users values(102, 'super.man@gmail.com', 'clark', 'kent', 'hero', 'super.man');
+insert into users values(103, 'thor@gmail.com', 'thor', 'odinson', 'hero', 'thor.odinson');
